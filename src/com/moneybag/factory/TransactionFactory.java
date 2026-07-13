6a704fb84a2263a8c9d374874c381f65786d19a0
thuhoai <dcvinh02@gmail.com>
2026-07-14 00:14:44 +0700

    Feat: Triển khai lớp TransactionFactory bằng cách áp dụng mẫu thiết kế Factory Method

---
+++ src/com/moneybag/factory/TransactionFactory.java
@@ -0,0 +1,43 @@
+package com.moneybag.factory;
+
+import com.moneybag.constant.Period;
+import com.moneybag.constant.TransactionType;
+import com.moneybag.model.*;
+import com.moneybag.wallet.Wallet;
+import java.time.LocalDate;
+import java.util.UUID; // thư viện tạo ID
+
+/**
+ * Khỏi tạo các đối tượng transaction phức tạp
+ */
+public class TransactionFactory {
+    // phương thức tính static khởi tạo giao dịch mà không cần khởi tạo Fatory
+    public static Transaction createTransaction(
+            TransactionType type,
+            double amount,
+            LocalDate date,
+            String note,
+            Category category,
+            Wallet wallet,
+            String extraInfo,
+            Period period // truyền vào chu kỳ nếu là khoản phí chi định kì, nếu không truyền thì null
+    ) {
+        // tự dộng sinh ra 1 ID random và duy nhất
+        String id = UUID.randomUUID().toString();
+        // Dựa vào loại giao dịch (type) để quyết định sẽ gọi Constructor của Class nào
+        switch (type) {
+            case INCOME:
+                return new Income(id, amount, date, note, category, wallet, extraInfo);
+
+                 case EXPENSE:
+                     // nếu truyền vào chu ký (period != null) thì tạo ra RecurringExpense
+                     if (period != null) {
+                         return new RecurringExpense(id, amount, date, note, category, wallet, extraInfo, period);
+                     }
+                     // nếu không có chu kỳ, tạo Expense bình thường
+                     return new Expense(id, amount, date, note, category, wallet, extraInfo);
+                     default:
+                         throw new IllegalAccessException("Loại giao dịch không được hệ thống hỗ trợ!");
+        }
+    }
+}
